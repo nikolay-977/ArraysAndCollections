30b4d3e28016d9089f491ba20c1f19c3d595a34a
Костин Николай Александрович <nkostin@samokat.ru>
2024-12-15 15:48:14 +0300

    add project

--- src/test/java/org/example/MyCollectionsTest.java
+++ src/test/java/org/example/MyCollectionsTest.java
@@ -19,7 +19,7 @@ class MyCollectionsTest {
         assertEquals(0, MyCollections.binarySearch(list, 1));
         assertEquals(7, MyCollections.binarySearch(list, 9));
 
-        // Элемента нет
+        // Элемент отсутствует
         assertEquals(-1, MyCollections.binarySearch(list, 0)); // до начала
         assertEquals(-9, MyCollections.binarySearch(list, 10)); // после конца
         assertEquals(-5, MyCollections.binarySearch(list, 5)); // между элементами
@@ -29,8 +29,10 @@ class MyCollectionsTest {
     void testBinarySearchWithComparator() {
         List<String> list = Arrays.asList("apple", "banana", "cherry", "date", "fig", "grape");
 
+        // Элемент присутствует
         assertEquals(1, MyCollections.binarySearch(list, "banana", Comparator.naturalOrder()));
         assertEquals(4, MyCollections.binarySearch(list, "fig", Comparator.naturalOrder()));
+
         // Элемент отсутствует
         assertEquals(-7, MyCollections.binarySearch(list, "kiwi", Comparator.naturalOrder()));
     }
